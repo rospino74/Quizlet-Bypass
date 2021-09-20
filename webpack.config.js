f356008f3dedca673dab9b56fd416578eb01765e
rospino74 <34315725+rospino74@users.noreply.github.com>
2021-09-20 20:29:33 +0200

    Creata nuova strategia build
    Ogni cartella in src/foreground dovrà avere un file structure.json che definisce:
        * entry: Un entry point nella cartella corrente
        * permissions: Array dei permessi
        * matches: Array di siti nei quali è richiesto lo script

--- webpack.config.js
+++ webpack.config.js
@@ -3,35 +3,57 @@ const fs = require('fs');
 const archiver = require('archiver');
 const WebpackObfuscator = require('webpack-obfuscator');
 const { EnvironmentPlugin } = require('webpack');
-const { version } = require('./package.json');
+const CopyPlugin = require('copy-webpack-plugin');
+const { version, description } = require('./package.json');
+const manifest = require('./public/manifest.json');
 
 // Path to the dist folder
 const distPath = path.resolve(__dirname, 'dist');
+const buildPath = path.resolve(distPath, 'build');
+const publicPath = path.resolve(__dirname, 'public');
+const srcForegroundPath = path.resolve(__dirname, 'src', 'foreground');
+
+// Array di informazioni sugli entrypoint
+const entryScripts = {};
+
+function buildManifest() {
+    // Leggo tutte le cartelle
+    fs.readdirSync(srcForegroundPath).forEach((directory) => {
+    // eslint-disable-next-line import/no-dynamic-require, global-require
+        const { permissions, matches, entry } = require(`${srcForegroundPath}/${directory}/structure.json`);
+
+        // Aggiungo informazioni sui permessi
+        manifest.permissions = [...new Set([...manifest.permissions, ...permissions])];
+
+        // e quelle sull'entrypoint
+        manifest.content_scripts.push({
+            matches,
+            js: [`${directory}.content.js`],
+        });
+        entryScripts[directory] = `./src/foreground/${directory}/${entry}`;
+    });
+
+    // Aggiorna versione e descrizione nel manifest
+    manifest.version = version;
+    manifest.description = description;
+
+    // salvo il manifest nella cartella di distribuzione
+    fs.writeFileSync(`${buildPath}/manifest.json`, JSON.stringify(manifest, null, 2));
+}
 
 async function zipExtensionFiles() {
     const archive = archiver('zip', { zlib: { level: 9 } });
-    const stream = fs.createWriteStream(`${__dirname}/extension.zip`);
+    const stream = fs.createWriteStream(`${distPath}/extension.zip`);
 
     return new Promise((resolve, reject) => {
         archive
-            .directory(distPath, false)
+            .directory(buildPath, false)
             .on('error', (err) => reject(err))
             .pipe(stream);
 
         stream.on('close', () => resolve());
         archive.finalize();
     });
-}
-
-function resolveAllForegroundScripts() {
-    const scripts = {};
-
-    // Leggo tutte le cartelle
-    fs.readdirSync(`${__dirname}/src/foreground`).forEach((directory) => {
-        scripts[directory] = `./src/foreground/${directory}/index.js`;
-    });
-
-    return scripts;
 }
 
 const commonConfig = {
@@ -81,19 +103,31 @@ const commonConfig = {
 
 const content = {
     ...commonConfig,
-    entry: resolveAllForegroundScripts,
+    entry: () => entryScripts,
     output: {
-        path: distPath,
         filename: '[name].content.js',
+        path: buildPath,
     },
+    plugins: [
+        {
+            apply: (compiler) => {
+                compiler.hooks.compile.tap('BuildManifest', buildManifest);
+            },
+        },
+        new CopyPlugin({
+            patterns: [
+                { from: `${publicPath}/icons`, to: `${buildPath}/icons` },
+            ],
+        }),
+    ],
 };
 
 const background = {
     ...commonConfig,
     entry: './src/background/background.ts',
     output: {
-        path: distPath,
         filename: 'background.js',
+        path: buildPath,
     },
 };
 
