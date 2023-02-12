4c598677fc2ccd1891e5cf79cbfdd1b91fb3a92c
Marko <34315725+rospino74@users.noreply.github.com>
2023-02-12 14:20:21 +0100

    Splitted getHTMLFromTranslate in various helper functions

--- src/frontend/latin/import/translateGrabber.ts
+++ src/frontend/latin/import/translateGrabber.ts
@@ -14,33 +14,49 @@ import UserAgent from 'random-useragent';
 import userSelectRemover from './userSelectRemover';
 import { makeWebRequest } from '../../common/makeWebRequest';
 
-const baseUrl = 'https://www-latin-it.translate.goog/';
-const getParams = '_x_tr_sl=it&_x_tr_tl=it&_x_tr_hl=it&_x_tr_pto=ajax,op,elem';
+const latinBaseUrl = 'https://www.latin.it/';
+const googleBaseUrl = 'https://www-latin-it.translate.goog/';
 
-export default async function getHTMLFromTranslate(url: string): Promise<string | null> {
-    // Prendo il contenuto da Google Traduttore
+function purifyHTML(html: string): string | undefined {
+    const template = document.createElement('template');
+    template.innerHTML = html;
+
+    // User select remover
+    const noCopyBoxes = template.content.querySelectorAll<HTMLElement>('[style*=user-select]');
+    userSelectRemover(noCopyBoxes);
+
+    // Scrape text from the second div ind the fifth child of the div with class 'corpo'
+    const body = template.content.querySelector<HTMLDivElement>('div.corpo > :nth-child(5) > div:nth-child(2)');
+    const content = body?.innerHTML;
+
+    // Replace the google url with the original url
+    return content?.replace(googleBaseUrl, latinBaseUrl);
+}
+
+async function getPageWithBaseAndParams(base: string, path: string, params = ''): Promise<string | undefined> {
+    let url = base + path;
+    if (params) {
+        url += (path.indexOf('?') !== -1 ? '&' : '?') + params;
+    }
+
     const headers = {
         'User-Agent': UserAgent.getRandom(),
         'Cache-Control': 'max-age=0',
         Pragma: 'no-cache',
     };
-
-    const response = await makeWebRequest(baseUrl + url + (url.indexOf('?') !== -1 ? '?' : '&') + getParams, 'GET', undefined, headers);
-
-    // Creo un elemento template
-    const template = document.createElement('template');
-    template.innerHTML = response;
-
-    // Rimuovo il no copy
-    const noCopyBoxes = template.content.querySelectorAll<HTMLElement>('[style*=user-select]');
-    userSelectRemover(noCopyBoxes);
-
-    // Prendo il contenuto del div con classe corpo
-    const body = template.content.querySelector<HTMLDivElement>('div.corpo');
-
-    // Prendo il contenuto del secondo div nel quinto elemento di body
-    const content = body?.children.item(4)?.children.item(1);
-
-    // Rimuovo url a google translate
-    return content?.innerHTML?.replace(baseUrl, '') ?? null;
+    const html = await makeWebRequest(url, 'GET', undefined, headers, false);
+    return purifyHTML(html);
 }
+
+export default async function getHTMLFromTranslate(url: string): Promise<string | undefined> {
+    // Google translate params
+    const getParams = {
+        '_x_tr_sl': 'it',
+        '_x_tr_tl': 'it',
+        '_x_tr_hl': 'it',
+        '_x_tr_pto': 'ajax,op,elem'
+    };
+    const getParamsUrl = Object.entries(getParams).map(([k, v]) => `${k}=${v}`).join('&');
+    return getPageWithBaseAndParams(googleBaseUrl, url, getParamsUrl);
+}
+
